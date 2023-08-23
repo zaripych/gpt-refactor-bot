0ef78fd1710e93685909a6e104c0ee4a2205f2fd
Rinat Zaripov <zaripov.rinat@gmail.com>
2023-08-23 23:00:24 +1000

    chore: polish the refactor start experience to a minimum

--- src/prompt/editor.ts
+++ src/prompt/editor.ts
@@ -1,14 +1,29 @@
 import { readFile } from 'fs/promises';
 
 import { spawnResult } from '../child-process/spawnResult';
+import { onceAsync } from '../utils/onceAsync';
+
+const hasCode = onceAsync(async () => {
+    const result = await spawnResult('which', ['code'], {
+        exitCodes: 'any',
+    });
+    return result.status === 0;
+});
 
 export const goToEndOfFile = async (file: string) => {
     const contents = await readFile(file, 'utf-8');
-    await spawnResult(
-        'code',
-        ['-g', `${file}:${contents.split('\n').length}:1`],
-        {
-            exitCodes: 'any',
-        }
-    );
+
+    if (await hasCode()) {
+        await spawnResult(
+            'code',
+            ['-g', `${file}:${contents.split('\n').length}:1`],
+            {
+                exitCodes: 'any',
+            }
+        );
+
+        return true;
+    }
+
+    return false;
 };
--- src/refactor/runRefactor.ts
+++ src/refactor/runRefactor.ts
@@ -1,25 +1,94 @@
 import fg from 'fast-glob';
+import { mkdir, writeFile } from 'fs/promises';
 import { basename, dirname } from 'path';
 import prompts from 'prompts';
+import { z } from 'zod';
 
 import { findRepositoryRoot } from '../file-system/findRepositoryRoot';
+import { goToEndOfFile } from '../prompt/editor';
 import { hasOneElement } from '../utils/hasOne';
 import { loadRefactorConfigs } from './loadRefactors';
 import { refactor } from './refactor';
 import type { RefactorConfig } from './types';
+
+const validateUsingSchema =
+    <T extends z.ZodType>(schema: T) =>
+    (value: z.infer<T>) => {
+        const result = schema.safeParse(value);
+        if (result.success) {
+            return true;
+        } else {
+            return result.error.issues[0]?.message || result.error.message;
+        }
+    };
 
 async function promptForConfig(refactors: RefactorConfig[]) {
     const answers = (await prompts({
         name: 'config',
         message: 'Select the refactor to run',
         type: 'select',
-        choices: refactors.map((config) => ({
-            title: config.name,
-            value: config,
-        })),
+        choices: [
+            ...refactors.map((config) => ({
+                title: config.name,
+                value: config,
+            })),
+            {
+                title: 'New refactor...',
+                value: 'new',
+            },
+        ],
     })) as {
-        config?: RefactorConfig;
+        config?: RefactorConfig | 'new';
     };
+
+    if (answers.config === 'new') {
+        const answers = (await prompts({
+            name: 'name',
+            message: 'Enter a short name for the refactor',
+            type: 'text',
+            validate: validateUsingSchema(
+                z.string().regex(/^[a-zA-Z0-9-]+$/, {
+                    message:
+                        'Must be a valid directory name matching /^[a-zA-Z0-9-]+$/',
+                })
+            ),
+        })) as {
+            name: string;
+        };
+
+        if (!answers.name) {
+            process.exit(0);
+        }
+
+        await mkdir(`.refactor-bot/refactors/${answers.name}`, {
+            recursive: true,
+        });
+
+        const goalMdFilePath = `.refactor-bot/refactors/${answers.name}/goal.md`;
+
+        await writeFile(
+            goalMdFilePath,
+            `
+\`\`\`yaml
+# For information about possible options have a look at the code:
+# https://github.com/zaripych/refactor-bot/blob/9b928d601a7586cd1adf20dbeb406625a0d7663f/src/refactor/types.ts#L11
+budgetCents: 100
+model: gpt-4
+\`\`\`
+
+> Please describe the refactoring in here, save the file and restart the command to continue...
+`,
+            'utf-8'
+        );
+
+        if (!(await goToEndOfFile(goalMdFilePath))) {
+            console.log(
+                `Please describe the refactoring in "${goalMdFilePath}", save the file and restart the command to continue...`
+            );
+        }
+
+        process.exit(0);
+    }
 
     if (!answers.config) {
         process.exit(0);
